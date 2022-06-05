fded12dc2a1160ee17b551726eb55898716a9318
0x3043 <neohex262@gmail.com>
2022-06-05 11:55:47 +0530

    Update bodi.js

--- pages/bodi.js
+++ pages/bodi.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div id="bodyy" className="flex flex-col items-center justify-center min-h-screen py-2">
 <Head>
-                <title>Binary Bodi</title>
+                <title>Genesis | Binary Bodi</title>
                 <link rel="icon" href="/images/favicon-32x32.png" />
                 <meta property="og:title" content="BinaryBodi"  key="ogtitle" />
                 <meta property="og:description" content="Curated collection of 7777 Bodi NFTs on Ethereum" key="ogdesc" />
@@ -22,82 +22,47 @@ export default function Home() {
               <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
                 <a href="/home" className="text-2xl Poppitandfinch text-white hover:text-black m-6">Home</a>
                 <a href="/mint" className="text-2xl Poppitandfinch text-white hover:text-black m-6">Mint</a>
-                <a href="/bodi" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Bodi</a>
-                <a href="/chapters" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Chapters</a>
+                <a href="/bodi" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Genesis</a>
+                <a href="/chapters" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Oracles</a>
                 <a href="faq" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
                 <a href="/roadmap" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Roadmap</a>
                 <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">About</a>
               </nav>
             </div>
           </div>
-  <div className="md:w-2/3 w-4/5 " id="about">
+          
+        <div className="md:w-1/2 w-4/5 " id="about">
           <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
-                  <div className="lg:w-1/3 w-3/4">  
+                  <div className="">  
+                  <br></br><br></br>
+         <h1 className="text-6xl montserrat text-blau ">Genesis Editions </h1>
+         
+            </div>
+            </div>
+            <div id="traits" className="flex flex-wrap justify-around  mx-6 py-6">
+              <p className="text-2xl text-white my-6  Poppitandfinch"> Genesis Issue of Bodi Oracle includes Seven Issues which is curated by the founders itself including the Art works and contents. Future Issues, contribution and direction of the Bodi Oracle is managed by the community. 
+ </p>
+             
+          </div>
+        </div>
+
+  <div className="md:w-1/2 w-4/5 " id="about">
+          <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
+                  <div className="">  
                   <br></br><br></br>
          <h1 className="text-6xl montserrat text-blau ">Bodi PFP </h1>
             </div>
             </div>
             <div id="traits" className="flex flex-wrap justify-around  mx-6 py-6">
-              <p className="text-xl text-white my-6  Poppitandfinch"> BinaryBodi is a character-driven, procedurally-generated tactical RPG. Like the best tabletop roleplaying experiences, Wildermyth gives you choices and answers your every decision with consequences that drive your characters forward.
-Lead a band of heroes as they grow from reluctant farmers into unique, legendary fighters. Combat unexpected threats and strange monsters across interactive battlefields. Unravel mysteries and share pensive moments in an ever-new fantasy setting that blends hard truths and sacrifice with humor and personal storytelling.
-Where does your myth lead? Come help us uncover it! Reminiscent of tabletop roleplaying, unique heroes are born in unique settings every game. They age, transform, fall in love, disagree, and make harrowing sacrifices.
-Each hero brings their own organic history and personality with them, but your choices and combat skills are what decide their paths and outcomes.
-All heroes die someday… but you get to hold on to your favorites. Reintroduce them in the next adventure, and over many lifetimes the myths you make will form your own legendary pantheon.
-</p>
+              <p className="text-2xl text-white my-6  Poppitandfinch"> A collection of 7777 Bodi  PFP NFTS made from a combination of 150 unique traits. Binary Bodi's lives on the Ethereum blockchain in form of ERC-721 tokens. There are no Bonding curves, price tiers etc. Minting  a Binary Bodi costs 0.077 ETH.  Bodi pfp is issued as Syndicate Pass which is the permanent access token of Binary Bodi RPG.  </p>
+              
+              <p className="text-2xl text-white my-6  Poppitandfinch"> Syndicate  members agreed to allow all other members to freely reprint their contents, to exchange rewards with each other, and to occasionally print a Issue of magazines and newspapers And anyone who agreed to those terms was allowed to join the syndicate.  </p>
+             
+          
           </div>
         </div>
-        <div className="md:w-2/3 w-4/5 " id="about">
-       <div id="gallery" className="   mx-6 py-6">
-                    <h2 className="text-blau Montserrat-Light text-7xl text-center">The Art</h2>
-                  <div className="flex flex-wrap  items-center mx-6   py-6 ">
-                  <div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">
+   
 
-                <div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">
-                                <div className=" p-4"><img src="images/1.png" alt="" width="110px" className="feature-image"/></div>
-                                <div className=" p-4"><img src="images/5.png" alt="" width="110px" className="feature-image"/></div>
-                                <div className=" p-4"><img src="images/4.png" alt="" width="110px" className="feature-image"/></div>
-                                <div className=" p-4"><img src="images/m4.png" alt="" width="110px" className="feature-image"/></div>
-                                <div className=" p-4"><img src="images/11.png" alt="" width="110px" className="feature-image"/></div>
-                                <div className=" p-4"><img src="images/15.png" alt="" width="110px" className="feature-image"/></div>
-                                </div><div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">
-                                <div className=" p-4"><img src="images/m3.png" alt="" width="110px" className="feature-image"/></div>
-                                <div className=" p-4"><img src="images/m6.png" alt="" width="110px" className="feature-image"/></div>
-                                <div className=" p-4"><img src="images/m11.png" alt="" width="110px" className="feature-image"/></div>
-                                <div className=" p-4"><img src="images/p29.png" alt="" width="110px" className="feature-image"/></div>
-                                <div className=" p-4"><img src="images/m17.png" alt="" width="110px" className="feature-image"/></div>
-                                <div className=" p-4"><img src="images/m23.png" alt="" width="110px"  className="feature-image"/></div>
-                                </div>
-                              
-                                <div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">
-                                   <div className=" p-4"><img src="images/c9.png" alt="" width="110px" className="feature-image"/></div>
-                                   <div className=" p-4"><img src="images/c7.png" alt="" width="110px" className="feature-image"/></div>
-                                   <div className=" p-4"><img src="images/c10.png" alt="" width="110px" className="feature-image"/></div>
-                                   <div className=" p-4"><img src="images/c20.png" alt="" width="110px" className="feature-image"/></div>
-                                   <div className=" p-4"><img src="images/m25.png" alt="" width="110px" className="feature-image"/></div>
-                                   <div className=" p-4"><img src="images/m16.png" alt="" width="110px"  className="feature-image"/></div>
-                                </div>
-                               
-
-                </div>
-</div>
-                </div>
-        </div>
-        <div className="md:w-2/3 w-4/5 " id="about">
-          <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
-                  <div className="lg:w-1/3 w-3/4">  
-                  <br></br><br></br>
-         <h1 className="text-6xl montserrat text-blau ">Syndicate Pass </h1>
-            </div>
-            </div>
-            <div id="traits" className="flex flex-wrap justify-around  mx-6 py-6">
-              <p className="text-xl text-white my-6  Poppitandfinch"> BinaryBodi is a character-driven, procedurally-generated tactical RPG. Like the best tabletop roleplaying experiences, Wildermyth gives you choices and answers your every decision with consequences that drive your characters forward.
-Lead a band of heroes as they grow from reluctant farmers into unique, legendary fighters. Combat unexpected threats and strange monsters across interactive battlefields. Unravel mysteries and share pensive moments in an ever-new fantasy setting that blends hard truths and sacrifice with humor and personal storytelling.
-Where does your myth lead? Come help us uncover it! Reminiscent of tabletop roleplaying, unique heroes are born in unique settings every game. They age, transform, fall in love, disagree, and make harrowing sacrifices.
-Each hero brings their own organic history and personality with them, but your choices and combat skills are what decide their paths and outcomes.
-All heroes die someday… but you get to hold on to your favorites. Reintroduce them in the next adventure, and over many lifetimes the myths you make will form your own legendary pantheon.
-</p>
-          </div>
-        </div>
     <div className="md:w-2/3 w-4/5 " id="about">
            <br></br>
            <div className="flex items-center justify-between w-full border-b-2	pb-6">
