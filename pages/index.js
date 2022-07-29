27ad2e8b20932764049008b2005b07f9ea247747
0x3043 <neohex262@gmail.com>
2022-07-29 11:03:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,11 @@ export default function Home() {
     <div id="bodyy" className="flex flex-col items-center justify-center min-h-screen py-2">
 <Head>
                 <title>Binary Bodi</title>
-                <link rel="icon" href="/images/favicon-32x32.png" />
+                <link rel="icon" href="/images/fab.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/fav144.ico"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/fab.ico"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico"/>
+                <link rel="manifest" href="/site.webmanifest"></link>
                 <meta property="og:title" content="BinaryBodi"  key="ogtitle" />
                 <meta property="og:description" content="Curated collection of 7777 Bodi NFTs on Ethereum" key="ogdesc" />
                 <meta property="og:type" content="website" key="ogtype" />
@@ -16,17 +20,17 @@ export default function Home() {
                 <meta name="twitter:title" content="BinaryBodi" key="twtitle" />
                 <meta name="twitter:description" content="Curated collection of 7777 Bodi NFTs on Ethereum " key="twdesc" />
 </Head>
-          <div >
+<div >
             <div className="flex items-center justify-between w-full border-b-2	pb-6">
-              <a href="/home" className=""><img src="images/lol1.png" width="110" alt="" className="logo-image" /></a><span></span>
+              <a href="/home" className=""><img src="images/logobodi.png" width="190" alt="" className="logo-image" /></a><span></span>
               <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
-                <a href="/home" className="text-2xl Poppitandfinch text-white hover:text-black m-6">Home</a>
-                <a href="/mint" className="text-2xl Poppitandfinch text-white hover:text-black m-6">Mint</a>
-                <a href="/bodi" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Genesis</a>
-                <a href="/syndicate" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Syndicate</a>
-                <a href="faq" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
-                <a href="/roadmap" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Roadmap</a>
-                <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">About</a>
+                <a href="/home" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">Home</a>
+                <a href="/mint" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">Mint</a>
+                <a href="/bodi" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">Bodi</a>
+                <a href="/syndicate" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">Syndicate </a>
+                <a href="faq" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
+                <a href="/roadmap" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">Roadmap</a>
+                <a href="/about" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">About</a>
               </nav>
             </div>
           </div>
@@ -36,10 +40,10 @@ export default function Home() {
                     <h1 className="text-5xl montserrat text-blau ">Binary Bodi</h1>
                     <p className="text-2xl text-white my-6  Poppitandfinch">A collaborative social expirement towards building a Metaverse Press Syndicate.</p>
                   </div>
-                  <img className="lg:w-1/2 w-3/4" src="images/dada.png" alt="" title="" width="300px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" src="images/bb.png" alt="" title="" width="300px" className="feature-image"/>
             </div>
                 <div id="traits" className="flex flex-wrap lg:flex-nowrap items-center justify-between w-full border-b-2	pb-6">
-              <img className="lg:w-1/2 w-3/4" src="images/maga.png" width="300px" className="feature-image"/>
+              <img className="lg:w-1/2 w-3/4" src="images/fewfw.png" width="300px" className="feature-image"/>
                
                   <div className=" sm:w-1/2 w-4/5 py-6 ">
                     <h3 className="text-blau montserrat text-5xl text-center">Bodi Oracle</h3>
@@ -54,25 +58,25 @@ export default function Home() {
                     <p className="text-2xl text-white my-6  Poppitandfinch">
                     A collection of 7777 Bodi PFP NFTS  on the Ethereum blockchain in form of ERC-721 tokens which acts as the syndicate pass.
                     </p> 
-                        <p class="detail">NFT <span class="name"> Bodi Syndic</span> </p>
-                    <p class="detail">Supply <span class="name"> 7777</span> </p>
-                    <p class="detail">Mint Price<span class="name"> 0.7ETH</span> </p>
-                    <p class="detail">Mint Date<span class="name"> TBA</span> </p>
-                    <p class="detail">Utility<span class="name"> Syndicate Pass</span></p>
+                        <p class="detail">NFT <span class="name">Bodi Syndic</span> </p>
+                    <p class="detail">Supply <span class="name">7777</span> </p>
+                    <p class="detail">Mint Price <span class="name">0.7ETH</span> </p>
+                    <p class="detail">Mint Date <span class="name">TBA</span> </p>
+                    <p class="detail">Utility <span class="name">Access Pass</span></p>
                     
                    </div>
-                   <img className="img" src="images/ghg.png" alt="" width="400px" className="feature-image"/> </div>
+                   <img className="img" src="images/46.png" alt="" width="450px" className="feature-image"/> </div>
       
                 </div>           
                 <div className="flex flex-wrap lg:flex-nowrap items-center justify-between w-full 	pb-6">
                 <div className="lg:w-2/2 w-/4">
               <br></br>       <p className="text-blau Montserrat-Light text-5xl">Metaverse Press Syndicate (MPS)</p>
-                     <p className="text-2xl text-white my-6  Poppitandfinch">Metaverse Press Syndicate is a network of countercultural newspapers and magazines  reflecting and shaping  the counte culture community in the Metaverse. MPS members agreed to allow all other members to freely reprint their contents, to exchange loyality rewards with each other. The MPS maintains an independent view. </p><br></br>               
+                     <p className="text-2xl text-white my-6  Poppitandfinch">Metaverse Press Syndicate is a network of countercultural newspapers and magazines  reflecting and shaping  the counter culture community in the Metaverse. </p><br></br>               
                     </div>
                 
                    </div>                 
                     <div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">                     
-                  <div className="border-4 border-blue-300 p-4"><img src="images/FFFSFS.png" alt="" width="300px" className="feature-image"layout="responsive" objectFit="contain"/></div>
+                  <div className="border-4 border-blue-300 p-4"><img src="images/full123.png" alt="" width="300px" className="feature-image"layout="responsive" objectFit="contain"/></div>
                   <div className="flex flex-col justify-between mx-6 sm:w-1/2 w-4/5 py-6 ">
                   <p className="text-blau Montserrat-Light text-2xl">Thematic Contents</p>
                     <p className="text-2xl text-white my-6  Poppitandfinch">Support all existing nft medias</p>
@@ -112,14 +116,15 @@ The Oracle  with roots in the Sixties underground press  is a colorful experimen
                    </div>  <div className="flex items-center justify-between w-full border-b-2	pb-6">
                       <a href="/home" className=""><img src="images/lol1.png" width="108" alt="" className="logo-image" /></a>
                       <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
-              <a href="/home" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Home</a>
-                <button type="button" className="text-2xl Poppitandfinchsans text-white  m-6">Mint</button>
-              <a href="/faq" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
-              <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">About US</a>
-              <a href="/province" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Province</a>
-              <a href="https://discord.gg/QtNPdpZKZE" className="text-2xl Poppitandfinchsans text-blau   m-6">Discord</a>
-              <a href="https://twitter.com/binarybodi" className="text-2xl Poppitandfinchsans text-blau  m-6">Twitter</a>
-              <a href="https://linktr.ee/binarybodi" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Communities</a>   
+                      <a href="/home" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">Home</a>
+                
+                <a href="faq" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
+                <a href="/roadmap" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">Roadmap</a>
+                <a href="/about" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">About</a>
+
+              <a href="https://discord.gg/QtNPdpZKZE" className="text-1xl Poppitandfinchsans text-blau   m-6">Discord</a>
+              <a href="https://twitter.com/binarybodi" className="text-1xl Poppitandfinchsans text-blau  m-6">Twitter</a>
+              <a href="https://linktr.ee/binarybodi" className="text-1xl Poppitandfinchsans text-white hover:text-black m-6">Communities</a>   
             </nav>
           </div>
         </div>
